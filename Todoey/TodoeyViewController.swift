2215ee575845cb5735452743c6e0d1e7f9f8deb6
Eunju Tak <teunju@gmail.com>
2018-06-29 17:03:30 +0900

    To do list complete with data source and delegate methods.

---
+++ Todoey/TodoeyViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Eunju Tak on 2018. 6. 29..
+//  Copyright © 2018년 Eunju Tak. All rights reserved.
+//
+
+import UIKit
+
+class TodoeyViewController: UITableViewController {
+
+    let itemArray = ["work","day off","save money","drive"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    //MARK - TavleView Datasource Methods
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+
+    //MARK - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+     //   print(itemArray[indexPath.row])
+        
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+    @IBAction func pressedAddButton(_ sender: UIBarButtonItem) {
+    }
+    
+    
+}
+
