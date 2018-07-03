c0b8c8d0f183209299c82da52b6641bf6d5ffa68
Eunju Tak <teunju@gmail.com>
2018-07-03 20:15:54 +0900

    Persisting data using Core Data.

--- Todoey/Controllers/TodoeyViewController.swift
+++ Todoey/Controllers/TodoeyViewController.swift
@@ -11,17 +11,23 @@ class TodoeyViewController: UITableViewController {
     
     var itemArray = [Item]()
     
+    var selectedCategory : Category?{
+        didSet{
+            loadItems()
+        }
+    }
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.Plist")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print(dataFilePath)
+       // print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         
-        loadItems()
     }
     
-    //MARK - TavleView Datasource Methods
+    //MARK: - TavleView Datasource Methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
@@ -33,39 +39,20 @@ class TodoeyViewController: UITableViewController {
         let item = itemArray[indexPath.row]
         
         cell.textLabel?.text = item.title
-        
-        //        Ternary operator
-        //        value = condition ? valueIfTrue : valueIfFalse
-        
+   
         cell.accessoryType = item.done ? .checkmark : .none
-        
-        //       ▲ same code ▼
-        //        if item.done == true{
-        //            cell.accessoryType = .checkmark
-        //        }else{
-        //            cell.accessoryType = .none
-        //        }
-        
+       
         return cell
     }
     
     
-    //MARK - TableView Delegate Methods
+    //MARK: - TableView Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //   print(itemArray[indexPath.row])
-        
         
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
         saveItems()
-        
-        //          ▲ same code ▼
-        //        if itemArray[indexPath.row].done == false{
-        //            itemArray[indexPath.row].done = true
-        //        }else{
-        //            itemArray[indexPath.row].done = false
-        //        }
         
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
             tableView.cellForRow(at: indexPath)?.accessoryType = .none
@@ -74,11 +61,9 @@ class TodoeyViewController: UITableViewController {
         }
         tableView.deselectRow(at: indexPath, animated: true)
         
-        
-        
     }
     
-    //MARK - Add New Items
+    //MARK: - Add New Items
     
     @IBAction func pressedAddButton(_ sender: UIBarButtonItem) {
         
@@ -88,9 +73,10 @@ class TodoeyViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what will happen once the user clicks the Add Item Button on our UIAlert
             
-            let newItem = Item()
+            let newItem = Item(context: self.context)
             newItem.title = textField.text!
-            
+            newItem.done = false
+            newItem.parentCategory = self.selectedCategory
             self.itemArray.append(newItem)
             
             self.saveItems()
@@ -100,42 +86,64 @@ class TodoeyViewController: UITableViewController {
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Creat New Item!"
             textField = alertTextField
-            
-            
+        
         }
         present(alert, animated: true, completion: nil)
         
     }
     
-    //MARK - Medel Manuplation Methods
+    //MARK: - Medel Manuplation Methods
     
     func saveItems(){
-        
-        let encoder = PropertyListEncoder()
-        
         do{
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+           try context.save()
         }catch{
-            print("Error encoding item array, \(error)")
+            print("Error saving context \(error)")
         }
         self.tableView.reloadData()
     }
     
-    func loadItems() {
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil) {
+
+        let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
         
-        if let data = try? Data(contentsOf: dataFilePath!){
-            let decoder = PropertyListDecoder()
-            do{
-            itemArray = try decoder.decode([Item].self, from: data)
-            }catch{
-                print("Error decoding item array, \(error)")
-            }
+        if let additionalPredicate = predicate {
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, additionalPredicate])
+        }else{
+            request.predicate = categoryPredicate
         }
         
+        do{
+       itemArray = try context.fetch(request)
+        }catch{
+            print("Error fetching data from context \(error)")
+        }
         
+        tableView.reloadData()
+    }
+}
+
+// MARK: - Search bar methods
+extension TodoeyViewController : UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        
+        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(with: request, predicate: predicate)
+    }
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0{
+            loadItems()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+        }
     }
     
     
 }
-
